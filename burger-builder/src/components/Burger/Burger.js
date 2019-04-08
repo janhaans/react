d6f291c9ba96a34fcf9beeb3ba1114cc411a45c6
Jan Haans <jan.haans@gmail.com>
2019-04-08 22:40:44 +0200

    Changed Burger Builder

---
+++ burger-builder/src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+
+    const ingredients = Object.keys(props.ingredients)
+        .map( ingredient => {
+            return [...Array(props.ingredients[ingredient])].map( (_, index) => {
+                return <BurgerIngredient key={ingredient+index} type={ingredient}/>;
+            });
+        });
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {ingredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+
+};
+
+export default burger;
