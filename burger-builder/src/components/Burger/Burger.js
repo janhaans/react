a2a789c4f0bb741b5557a4f1695f878616e9777f
Jan Haans <jan.haans@gmail.com>
2019-04-12 21:44:41 +0200

    Added PropType validation

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.module.css';
 
 const burger = (props) => {
 
-    const ingredients = Object.keys(props.ingredients)
+    let ingredients = Object.keys(props.ingredients)
         .map( ingredient => {
             return [...Array(props.ingredients[ingredient])].map( (_, index) => {
                 return <BurgerIngredient key={ingredient+index} type={ingredient}/>;
             });
-        });
+        })
+        .reduce( (acc, cur) => acc.concat(cur), []);
+
+    if (ingredients.length === 0) {
+        ingredients = <p>You have forgotten to specify ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
