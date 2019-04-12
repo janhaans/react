1dce6b7f92e530d44a42c1972ef2e4f04b11c327
Jan Haans <jan.haans@gmail.com>
2019-04-12 23:39:50 +0200

    Added Burger Build Controls

--- burger-builder/src/containers/Burgerbuilder/Burgerbuilder.js
+++ burger-builder/src/containers/Burgerbuilder/Burgerbuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENTS_PRICES = {
+    salad: 0.2,
+    bacon: 0.5,
+    chees: 0.4,
+    meat: 1.3
+};
 
 class Burgerbuilder extends Component {
 
@@ -10,14 +18,46 @@ class Burgerbuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
+        },
+        price: 4
+    }
+
+    addIngredient = (type) => {
+        let updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type]++;
+        let updatedPrice = this.state.price + INGREDIENTS_PRICES[type];
+        this.setState({
+            ingredients: updatedIngredients,
+            price: updatedPrice
+        });
+    }
+
+    removeIngredient = (type) => {
+        let updatedIngredients = {...this.state.ingredients};
+        if (updatedIngredients[type]>0) {
+            updatedIngredients[type]--;
+            let updatedPrice = this.state.price +INGREDIENTS_PRICES[type];
+            this.setState({
+                ingredients: updatedIngredients,
+                price: updatedPrice
+            });
         }
     }
 
     render() {
+
+        const disabledKeys = {...this.state.ingredients};
+        for (let key in disabledKeys) {
+            disabledKeys[key] = disabledKeys[key] <= 0;
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls 
+                    ingredientAdded={this.addIngredient}
+                    ingredientRemoved={this.removeIngredient}
+                    disabledKeys = {disabledKeys}
+                />
             </Aux>
         );
     };    
